3f1263254b016918a507adb850a791b942f98aa5
Jordan Mynes <mynesjordan@gmail.com>
2018-04-06 18:53:10 -0500

    Reverted to JS, now works!

--- js/materialize.js
+++ js/materialize.js
@@ -1,8 +1,4 @@
+M.AutoInit();
+
 var elem = document.querySelector('.sidenav');
-var instance = M.Sidenav.init(elem, options);
-
-// Or with jQuery
-
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-});
+  var instance = M.Sidenav.init(elem, options);
